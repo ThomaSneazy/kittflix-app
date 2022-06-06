f79e8bdea8e7694eb2fb117b6ce40a794e18aa91
fatoudiallo <fatous.diallo@yahoo.fr>
2022-06-06 17:02:58 +0200

    adding reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_142017) do
+ActiveRecord::Schema.define(version: 2022_06_06_150000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2022_06_06_142017) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["vehicle_id"], name: "index_bookings_on_vehicle_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "vehicle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["vehicle_id"], name: "index_reviews_on_vehicle_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,5 +67,6 @@ ActiveRecord::Schema.define(version: 2022_06_06_142017) do
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "vehicles"
+  add_foreign_key "reviews", "vehicles"
   add_foreign_key "vehicles", "users"
 end
