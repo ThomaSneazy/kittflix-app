48e6ca32e0c974bd33907faee5c7811fa73c566f
Nicolas Ruff <nicoff.ruff@gmail.com>
2022-06-06 16:17:26 +0200

    not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_133240) do
+ActiveRecord::Schema.define(version: 2022_06_06_134742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 2022_06_06_133240) do
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price_per_day"
+    t.integer "year"
+    t.string "category"
+    t.text "descritpion"
+    t.boolean "available", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
