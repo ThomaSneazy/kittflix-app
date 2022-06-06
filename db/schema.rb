ecd07b601983d772ffafc9718d63eb2a1b0c9fb3
fatoudiallo <fatous.diallo@yahoo.fr>
2022-06-06 16:22:31 +0200

    foreign_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_134742) do
+ActiveRecord::Schema.define(version: 2022_06_06_142017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 2022_06_06_134742) do
     t.boolean "available", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
+  add_foreign_key "vehicles", "users"
 end
