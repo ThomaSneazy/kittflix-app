d2095fc2a0e95ea64a4a19fcab09a75d84b065ab
FatFatFr <101579962+FatfatFr@users.noreply.github.com>
2022-06-07 11:24:20 +0200

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :vehicule
+  belongs_to :vehicle
   has_many :users, through: :vehicles
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
