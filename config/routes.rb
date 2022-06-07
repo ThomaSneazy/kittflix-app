d6571099e915dd43186d4249b30124b0de9a73a9
Thomas Fouquert <tfouquert@gmail.com>
2022-06-07 10:28:00 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'vehicles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :vehicles do
+    resources :bookings, only: %i[new create edit update]
+    resources :reviews, only: %i[new create edit update]
+  end
+  resources :bookings, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
