6bd117c86334c5694f9918733ca5900ef5fea686
Thomas Fouquert <tfouquert@gmail.com>
2022-06-09 16:59:56 +0200

    default avatar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,6 @@ require 'open-uri'
 
 class ProfilesController < ApplicationController
   def show
-    avatar = URI.open('https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg?20160314221008')
-    @default_image = current_user.photo.attach(io: avatar, filename: "1.jpeg", content_type: 'image/jpeg') unless current_user.photo.key
+
   end
 end
